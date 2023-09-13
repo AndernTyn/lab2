b79f535b5480b3eb66167e14370a155a4b31ad4a
Туняк Андрей <tyn010101@mail.ru>
2023-09-13 17:07:33 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Туняк Андрей Николаевич, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+         &copy; Туняк Андрей, ФБИ-13, 3 курс, 2023
+         </footer>
+    </body>
+</html>
+"""
