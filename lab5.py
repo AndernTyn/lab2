a17e0d3011cb0f4feeb9c702f69971346c367f48
Светлана Матвеева <staruk.007@mail.ru>
2023-11-19 17:37:02 +0700

    Вывод пользователей

--- lab5.py
+++ lab5.py
@@ -1,33 +1,72 @@
-from flask import redirect, render_template, request, Blueprint 
+from flask import Flask, render_template, Blueprint
 import psycopg2
 
+app = Flask(__name__)
 
-lab5 =Blueprint("lab5", __name__)
+# Создаем Blueprint с именем "lab5"
+lab5 = Blueprint("lab5", __name__)
 
-@lab5.route("/lab5")
-def main():
-# Прописываем параметры подключения к БД 
+# Функции подключения и закрытия базы данных остаются без изменений
+def dbConnect():
     conn = psycopg2.connect(
-        host="127.0.0.1", 
-        database="Knowledge_base_for_Tunyak", 
+        host="127.0.0.1",
+        database="knowledge_base_for_tunyak",
         user="tunyak_knowledge_base",
         password="123")
+    return conn
 
-    # Получаем курсор. С помощью него мы можем выполнять SQL-запросы 
+def dbClose(cursor, connection):
+    cursor.close()
+    connection.close()
+
+# Маршрут для вывода всех пользователей в консоль
+@lab5.route("/lab5")
+def main():
+    # Устанавливаем соединение с базой данных
+    conn = dbConnect()
+
+    # Создаем курсор для выполнения SQL-запросов
     cur = conn.cursor()
 
-    # Пишем запрос, который psycopg2 должен выполнить 
+    # Выполняем запрос к базе данных для получения всех пользователей
     cur.execute("SELECT * FROM users;")
-
-    #fetchall - получить все строки, которые получились в результате
-    #выполнения SQL-запроса в execute
-    # Сохраняем эти строки в переменную result 
+    
+    # Получаем все строки с результатами запроса
     result = cur.fetchall()
-
-    # Закрываем соединение с БД 
-    cur.close() 
-    conn.close()
-
+    
+    # Выводим результат в консоль (это можно убрать в боевом приложении)
     print(result)
+    
+    # Закрываем курсор и соединение с базой данных
+    dbClose(cur, conn)
+    
+    # Возвращаем строку, которая сообщает пользователю перейти в консоль (это можно изменить)
+    return "перейдите в консоль"
 
-    return "go to console"
+# Маршрут для вывода имен пользователей в HTML
+@lab5.route("/lab5/users")
+def show_users():
+    # Устанавливаем соединение с базой данных
+    conn = dbConnect()
+
+    # Создаем курсор для выполнения SQL-запросов
+    cur = conn.cursor()
+
+    # Выполняем запрос к базе данных для получения всех пользователей
+    cur.execute("SELECT * FROM users;")
+    
+    # Получаем все строки с результатами запроса
+    result = cur.fetchall()
+    
+    # Закрываем курсор и соединение с базой данных
+    dbClose(cur, conn)
+
+    # Рендерим HTML-шаблон "users.html" с данными пользователей
+    return render_template("users.html", users=result)
+
+# Регистрация Blueprint в приложении Flask
+app.register_blueprint(lab5)
+
+if __name__ == "__main__":
+    # Запуск приложения Flask в режиме отладки
+    app.run(debug=True)
