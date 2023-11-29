367ba5011f49fe59a2d292ca468822a760f9c5e9
Светлана Матвеева <staruk.007@mail.ru>
2023-11-29 19:43:01 +0700

    Промежуточное сохранение 2

--- lab6.py
+++ lab6.py
@@ -1,13 +1,23 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, session
 from Db import db
 from Db.models import users, articles
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user
 
 lab6 = Blueprint("lab6", __name__)
 
-@lab6.route("/lab6/check")
+@lab6.route("/lab6")
 def main():
+    user_is_authenticated = 'user_id' in session
+    current_user_info = {"username": "Гость"}
+
+    if user_is_authenticated:
+        current_user_info["username"] = session["username"]
+
+    return render_template("lab6.html", user_is_authenticated=user_is_authenticated, current_user=current_user_info)
+
+@lab6.route("/lab6/check")
+def main1():
     my_users = users.query.all()
     print(my_users)
     return "result in console!"
@@ -19,7 +29,6 @@ def mainart():
         print(f"{article_item.title}-{article_item.article_text}")
     return "Result in console!"
 
-
 @lab6.route("/lab6/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
@@ -28,13 +37,13 @@ def register():
     username_form = request.form.get("username")
     password_form = request.form.get("password")
 
-    isUserExist = users.query.filter_by(username = username_form).first()
+    isUserExist = users.query.filter_by(username=username_form).first()
 
     errors = []
 
     if isUserExist is not None:
         errors.append("Такой пользователь уже существует!")
-        return render_template("register6.html", errors = errors)
+        return render_template("register6.html", errors=errors)
     elif not username_form:
         errors.append("Введите имя пользователя!")
         return render_template("register6.html", errors=errors)
@@ -44,10 +53,9 @@ def register():
 
     hashedPswd = generate_password_hash(password_form, method="pbkdf2")
 
-    newUser = users(username = username_form, password = hashedPswd)
+    newUser = users(username=username_form, password=hashedPswd)
 
     db.session.add(newUser)
-
     db.session.commit()
 
     return redirect("/lab6/login")
@@ -62,20 +70,38 @@ def login():
     username_form = request.form.get("username")
     password_form = request.form.get("password")
 
-    my_user = users.query.filter_by(username = username_form).first()
+    my_user = users.query.filter_by(username=username_form).first()
 
-    if not (username_form or password_form):
+    if not (username_form and password_form):
         errors.append("Введите имя пользователя и пароль!")
-        return render_template("login6.html", errors = errors)
+        return render_template("login6.html", errors=errors)
     elif my_user is None:
-        errors.append("Такого пользователя не существует! Зарегестрируйтесь!")
-        return render_template("login6.html", errors = errors)
-    elif my_user is not check_password_hash(my_user.password, password_form):
+        errors.append("Такого пользователя не существует! Зарегистрируйтесь!")
+        return render_template("login6.html", errors=errors)
+    elif not check_password_hash(my_user.password, password_form):
         errors.append("Введите правильный пароль!")
-        return render_template("login6.html", errors = errors)
+        return render_template("login6.html", errors=errors)
     elif my_user is not None:
         if check_password_hash(my_user.password, password_form):
             login_user(my_user, remember=False)
-            return redirect("/lab6/articles")
+            return redirect("/lab6")
     
     return render_template("login6.html")
+
+@lab6.route("/lab6/articles")
+@login_required
+def articles_list():
+    my_articles = articles.query.filter_by(user_id=current_user.id).all()
+    return render_template("list_articles.html", articles=my_articles)
+
+@lab6.route("/lab6/articles/<int:article_id>")
+@login_required
+def article_details(article_id):
+    article = articles.query.get_or_404(article_id)
+    return render_template("article_details.html", article=article)
+
+@lab6.route ("/lab6/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect ("/lab6")
